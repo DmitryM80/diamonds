1bcc48de14898b2a5b0b9d280bec4565849c2ef5
DmitryM80 <mihalych80@gmail.com>
2021-03-07 22:51:27 +0500

    Избранное - добавление, караты

--- catalog/view/theme/bfr/javascript/common.js
+++ catalog/view/theme/bfr/javascript/common.js
@@ -135,16 +135,8 @@ var wishlist = {
 			data: 'product_id=' + product_id,
 			dataType: 'json',
 			success: function(json) {
-				// $('.alert-dismissible').remove();
-				console.log(json);
-
-				if (json['redirect']) {
-					// location = json['redirect'];
-				}
 
 				if (json['success']) {
-					// $('#content').parent().before('<div class="alert alert-success alert-dismissible"><i class="fa fa-check-circle"></i> ' + json['success'] + ' <button type="button" class="close" data-dismiss="alert">&times;</button></div>');
-          			console.log('wishlist_success');
 					$('#wishlist-added').modal();
 
 					setTimeout(function() {
@@ -152,11 +144,6 @@ var wishlist = {
 						$('#favorite-ico').show();
 					}, 1500);
 				}
-
-				// $('#wishlist-total span').html(json['total']);
-				// $('#wishlist-total').attr('title', json['total']);
-
-				// $('html, body').animate({ scrollTop: 0 }, 'slow');
 			},
 			error: function(xhr, ajaxOptions, thrownError) {
 				alert(thrownError + "\r\n" + xhr.statusText + "\r\n" + xhr.responseText);
@@ -177,39 +164,14 @@ var cart = {
 			type: 'post',
 			data: 'product_id=' + product_id + '&quantity=' + (typeof(quantity) != 'undefined' ? quantity : 1),
 			dataType: 'json',
-			beforeSend: function() {
-				// $('#cart > button').button('loading');
-			},
-			complete: function() {
-				// $('#cart > button').button('reset');
-			},
 			success: function(json) {
-				// $('.alert-dismissible, .text-danger').remove();
-
-				if (json['redirect']) {
-					// location = json['redirect'];
-				}
-
 				if (json['success']) {
-					// $('#content').parent().before('<div class="alert alert-success alert-dismissible"><i class="fa fa-check-circle"></i> ' + json['success'] + ' <button type="button" class="close" data-dismiss="alert">&times;</button></div>');
-					// console.log('success', json);
 					// Need to set timeout otherwise it wont update the total
 					setTimeout(function () {
-						// $('#cart > button').html('<span id="cart-total"><i class="fa fa-shopping-cart"></i> ' + json['total'] + '</span>');
 						$('#items-in-cart').text(json['products_qty']);
 						$('#items-in-cart').show();
-						// $('#footerLine span.mark').text(json['products_qty']);
-
 						$('#add-to-cart-modal').modal();
 					}, 100);
-
-					// $('html, body').animate({ scrollTop: 0 }, 'slow');
-
-					// $('#cart > ul').load('index.php?route=common/cart/info ul li');
-
-
-
-
 				}
 			},
 			error: function(xhr, ajaxOptions, thrownError) {
